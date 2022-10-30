9a5d04bf3647b4787093e7fcea933f650eaf00da
rico <h.nacmer@gmail.com>
2022-10-30 16:38:25 +0100

    prawie gotowe

--- Generator.py
+++ Generator.py
@@ -7,17 +7,18 @@ T0 = datetime.datetime(2001,1,1)
 T1 = datetime.datetime(2011,6,30)
 T2 = datetime.datetime(2022,12,31)
 
-dates = pandas.date_range(T0,T1,periods=20) #,freq="M"
+dates = pandas.date_range(T0,T1,periods=25000) #,freq="M"
 print(dates)
 
-directors = [("Martin","Wilder"),("Anya","Holding"),("Annabella","Livingston"),("Kendal","Millington"),("Seamus","Howe")]
-equipement = ["Armata","Trapez","Lina do chodzenia","Huśtawka","Szczudła",
+directors_name = [("Martin","Wilder"),("Anya","Holding"),("Annabella","Livingston"),("Kendal","Millington"),("Seamus","Howe")]
+equipement_name = ["Armata","Trapez","Lina do chodzenia","Huśtawka","Szczudła",
 "Miecz do Połykania","Olej Parafinowy","Kije Do żonglerki","Piłeczki do Żonglerki",
 "Monocykl","Trampolina","Basen i Deska do Skakania","Skrzynia do przecinania ludzi"]
-location = ["Gdańsk","Poznań","Warszawa","Kraków","Szczecin","Olsztyn","Frombork","Łódź","Lublin","Gdynia","Sopot","Białystok"]
-act_name = []
-act_desc = []
-incidents_report = ["Złamana Ręka","Złamana noga","Rany cięte","Poparzenia","Skręcona kostka","Zwichnięta ręka"]
+location_name = ["Gdańsk","Poznań","Warszawa","Kraków","Szczecin","Olsztyn","Frombork","Łódź","Lublin","Gdynia","Sopot","Białystok"]
+act_name = ["Wystrzał z armaty","Sztuczki na trapezie","Chodzenie po linie","Huśtanie Ekstremalne","Chodzenie na szczudłach",
+"Połykanie miecza","Plucie ogniem","Żonglerka Kijami","Żonglerka Kulami","Jazda na monocyklu","Wysokie skoki na trampolinie","Skoki do wody","Sztuczka z przecinaniem"]
+act_desc = ["..."]
+incidents_report = [["Złamana Ręka","Złamana noga","Rany cięte","Poparzenia","Skręcona kostka","Zwichnięta ręka"]]
 
 
 class Performance:
@@ -26,6 +27,15 @@ class Performance:
         self.art_dir_id = art_dir_id
         self.time = time
         self.location = location
+    
+    def SQL(self):
+        txt = "INSERT INTO performances (id,artistic_director_id,time,location) VALUES ("
+        txt += str(self.id)+ ","  
+        txt += str(self.art_dir_id)+ ","
+        txt += str(self.time)+ ","
+        txt += "'" + str(self.location) + "'"
+        txt += ")\n"
+        return txt
 
 class Act:
     def __init__(self,id,performance_id,name,desc):
@@ -34,22 +44,50 @@ class Act:
         self.name = name
         self.desc = desc
 
+    def SQL(self):
+        txt = "INSERT INTO acts (id,performance_id,name,decription) VALUES ("
+        txt += str(self.id)+ ","  
+        txt += str(self.performance_id)+ ","
+        txt += "'" + str(self.name)+ "',"
+        txt += "'" + str(self.desc) + "'"
+        txt += ")\n"
+        return txt
+
 class Act_eq:
     def __init__(self,eq_id,act_id):
         self.eq_id = eq_id
         self.act_id = act_id
 
+    def SQL(self):
+        txt = "INSERT INTO act_equipement (equipement_id,act_id) VALUES ("
+        txt += str(self.eq_id)+ ","  
+        txt += str(self.act_id)
+        txt += ")\n"
+        return txt
 
 class Equipement:
     def __init__(self,id,name):
         self.id = id
         self.name = name
 
+    def SQL(self):
+        txt = "INSERT INTO equipements (id,name) VALUES ("
+        txt += str(self.id)+ ","  
+        txt += "'" + str(self.name)+ "'"
+        txt += ")\n"
+        return txt
+
 class Act_artist:
     def __init__(self,artist_id,act_id):
         self.artist_id = artist_id
         self.act_id = act_id
 
+    def SQL(self):
+        txt = "INSERT INTO artist_act (equipement_id,act_id) VALUES ("
+        txt += str(self.artist_id)+ ","  
+        txt += str(self.act_id)
+        txt += ")\n"
+        return txt
 
 class Artist:
     def __init__(self,id,name,surname,pseudonym):
@@ -58,33 +96,103 @@ class Artist:
         self.surname = surname
         self.pseudonym = pseudonym
 
+    def SQL(self):
+        txt = "INSERT INTO acts (id,name,surname,pseudonym) VALUES ("
+        txt += str(self.id)+ ","          
+        txt += "'" + str(self.name)+ "',"
+        txt += "'" + str(self.surname)+ "',"
+        txt += "'" + str(self.pseudonym) + "'"
+        txt += ")\n"
+        return txt
+
 class Incident:
     def __init__(self,id,type,report,act_id):
         self.id = id
         self.type = type
         self.report = report
         self.act_id = act_id
-
+    
+    def SQL(self):
+        txt = "INSERT INTO acts (id,name,surname,pseudonym) VALUES ("
+        txt += str(self.id)+ ","          
+        txt += "'" + str(self.type)+ "',"
+        txt += "'" + str(self.report)+ "',"
+        txt += str(self.act_id) 
+        txt += ")\n"
+        return txt
 
 class Artistic_director:
     def __init__(self,id,name,surname):
         self.id = id
         self.name = name
         self.surname = surname
+    
+    def SQL(self):
+        txt = "INSERT INTO acts (id,name,surname,pseudonym) VALUES ("
+        txt += str(self.id)+ ","          
+        txt += "'" + str(self.name)+ "',"
+        txt += "'" + str(self.surname)+ "'"
+        txt += ")\n"
+        return txt
 
 
-directors_c = []
+directors = []
 performances =[]
+acts = []
+
+actors = []
+equipement = []
+
+eq_acts = []
+art_acts = []
+injuries = []
 
 i = 0
-for dir in directors:
-    directors_c.append(Artistic_director(i,dir[0],dir[1]))
+for dir in directors_name:
+    directors.append(Artistic_director(i,dir[0],dir[1]))
     i+= 1
 
 i = 0
+for eq in equipement_name:
+    directors.append(Equipement(i,eq))
+    i+= 1
+
+i = 0
+for eq in equipement_name:
+    actors.append(Artist(i,"a","b","c"))
+    i+= 1
+
+
+
+i = 0
 for date in dates:
-    performances.append(Performance(i,randbelow(len(directors_c)),date,location[randbelow(len(location))]))
+    performances.append(Performance(i,randbelow(len(directors)),date,location_name[randbelow(len(location_name))]))
     i+=1
 
 
-    
+
+i = 0
+for performance  in performances: #dodac csvke
+    performance : Performance
+    for x in range(3): 
+        random_nr = randbelow(len(act_name))
+        acts.append(Act(i,performance.id,act_name[random_nr],act_desc[0]))
+        eq_acts.append(Act_eq(random_nr,i))
+        art_acts.append(Act_artist(randbelow(len(actors)),i))
+        i+=1
+
+
+file = open("SQL_Queries.sql","w")
+
+txt = ""
+for x in directors:
+    txt+= x.SQL()
+txt+="\n"
+
+for x in performances:
+    txt+= x.SQL()
+
+
+file.write(txt)
+file.close()
+print("X")
