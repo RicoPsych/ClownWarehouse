ad077b65acc7708868420f3112b7c67390d8c6d0
rico <h.nacmer@gmail.com>
2022-11-27 15:28:29 +0100

    bryhsad

--- SQL_CreateWarehouse.sql
+++ SQL_CreateWarehouse.sql
@@ -2,14 +2,12 @@
 */
 CREATE TABLE artisitc_directors (
 	id int,
-	name varchar(255),
-	surname varchar(255),
+	name_surname varchar(255),
 	PRIMARY KEY (id));
 
 CREATE TABLE artists (
 	id int,
-	name varchar(255),
-	surname varchar(255),
+	name_surname varchar(255),
 	pseudonym varchar(255),
 	PRIMARY KEY (id));
 
@@ -17,15 +15,26 @@ CREATE TABLE equipment (
 	id int,
 	name varchar(255),
 	producer varchar(255),
+	is_current bit,
 	PRIMARY KEY (id));
 
 CREATE TABLE performances (
     id Int,
     artistic_director_id Int,
-    time date,
+    date Int,
     location varchar(255),
 	PRIMARY KEY (id),
 	FOREIGN KEY (artistic_director_id) REFERENCES artisitc_directors(id)
+);
+
+CREATE TABLE Date (
+    id Int,
+    year Int,
+	month varchar(12),
+    day Int,
+    day_of_week varchar(15),
+	PRIMARY KEY (id)
+
 );
 
 
@@ -35,22 +44,24 @@ CREATE TABLE acts (
 	performance_id Int,
 	artist_id Int,
 	equipment_id Int,
-	name varchar(255),
+	accident_id Int,
 	description varchar(255),
-	
+	grade float,
+	surveyed Int,
+	accidents_num Int,
+
 	PRIMARY KEY (id), 
 	FOREIGN KEY (performance_id) REFERENCES performances(id),
 	FOREIGN KEY (artist_id) REFERENCES artists(id),
-	FOREIGN KEY (equipment_id) REFERENCES equipment(id)
+	FOREIGN KEY (equipment_id) REFERENCES equipment(id),
+	FOREIGN KEY (accident_id) REFERENCES acidents(id)
 	);
 
 CREATE TABLE accidents (
 	id int,
 	type int,
 	report varchar(255),
-	act_id int,
 	PRIMARY KEY (id), 
-	FOREIGN KEY (act_id) REFERENCES acts(id));
 
 -- CREATE TABLE act_equipment (
 -- 	equipment_id Int,
